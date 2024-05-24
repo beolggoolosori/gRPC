11a1755cbbb51285de0a52a731a3e87805f1a646
Bellhoon-__- <122255342+beolggoolosori@users.noreply.github.com>
2024-05-24 13:16:26 +0900

    init: gRPC Client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcHelloWorld;
+
+
+// See https://aka.ms/new-console-template for more information
+using var channel = GrpcChannel.ForAddress("http://localhost:5211");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+Console.WriteLine("Greeting: " + reply.Message);
